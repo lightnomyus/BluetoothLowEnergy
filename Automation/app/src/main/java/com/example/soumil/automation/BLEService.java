16398346f5bfcba9e3a11c9638c8bb52adfe644d
SoumilChugh <schugh@janacare.com>
2017-12-06 18:47:07 +0530

    Started adding Bluetooth code

--- Automation/app/src/main/java/com/example/soumil/automation/BLEService.java
+++ Automation/app/src/main/java/com/example/soumil/automation/BLEService.java
@@ -2,23 +2,60 @@ package com.example.soumil.automation;
 
 import android.app.Service;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCharacteristic;
 import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanFilter;
+import android.bluetooth.le.ScanRecord;
+import android.bluetooth.le.ScanResult;
+import android.bluetooth.le.ScanSettings;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Binder;
 import android.os.IBinder;
 import android.os.Messenger;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
+import android.widget.Toast;
 
-public class BLEService extends Service {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.UUID;
+
+public class BLEService extends Service  {
+
 
     public enum BLEState {SCANNING, CONNECTED, DISCONNECTED};
     public BLEState mBLEState;
     private final IBinder mBinder = new BLEServiceBinder();
     private Messenger mMessenger;
-    BluetoothAdapter mBluetoothAdapter;
-    BluetoothManager mBluetoothManager;
+
     public static boolean isServiceBinded;
     public static final String TAG = BLEService.class.getName();
+
+    BluetoothAdapter mBluetoothAdapter;
+    BluetoothManager mBluetoothManager;
+    private BluetoothLeScanner mBluetoothLeScanner;
+    private ScanSettings mScanSettings;
+    private List<ScanFilter> mScanListFiler;
+    private BluetoothGatt mBluetoothGatt;
+    private BluetoothGattCharacteristic mBluetoothGattCharacteristic;
+    private static String CLIENT_CHARACTERISTIC_CONFIGUARION_DESCRIPTOR = "00002902-0000-1000-8000-00805f9b34fb";
+    private static String JANACARE_DATA_SERVICE = "82fcabcd-8ebd-4d31-b978-fc7bacebebde";
+    private static String READ_CHARACTERISTIC = "82fcabce-8ebd-4d31-b978-fc7bacebebde";
+    private static String WRITE_CHARACTERISTIC = "82fcabcf-8ebd-4d31-b978-fc7bacebebde";
+    private static UUID READ = UUID.fromString(READ_CHARACTERISTIC);
+    private static UUID WRITE = UUID.fromString(WRITE_CHARACTERISTIC);
+    private static UUID SERVICE = UUID.fromString(JANACARE_DATA_SERVICE);
+    private static UUID CCCD = UUID.fromString(CLIENT_CHARACTERISTIC_CONFIGUARION_DESCRIPTOR);
+    private BluetoothStateReceiver mBluetoothStateReceiver;
+
     public BLEService() {
 
     }
@@ -27,6 +64,17 @@ public class BLEService extends Service {
     public IBinder onBind(Intent intent) {
         isServiceBinded = true;
         mBluetoothManager = (BluetoothManager) getApplicationContext().getSystemService(Context.BLUETOOTH_SERVICE);
+        Toast.makeText(getApplicationContext(),"Service is binded",Toast.LENGTH_SHORT).show();
+        IntentFilter filter = new IntentFilter();
+        Log.d(TAG,"Service Started");
+        filter.addAction(mBluetoothAdapter.ACTION_STATE_CHANGED);
+        filter.addAction(BluetoothDevice.ACTION_BOND_STATE_CHANGED);
+        filter.addAction("android.action.scan");
+        filter.setPriority(IntentFilter.SYSTEM_HIGH_PRIORITY);
+        mBluetoothStateReceiver = new BluetoothStateReceiver();
+        registerReceiver(mBluetoothStateReceiver,filter);
+
+        startScan();
 
         return mBinder;
     }
@@ -35,6 +83,11 @@ public class BLEService extends Service {
     public boolean onUnbind(Intent intent) {
         super.onUnbind(intent);
         isServiceBinded = false;
+        Toast.makeText(getApplicationContext(),"Service is Unbinded",Toast.LENGTH_SHORT).show();
+        unregisterReceiver(mBluetoothStateReceiver);
+        Intent intent1 = new Intent("android.action.service").putExtra("unBind","Service Unbinded");
+        sendBroadcast(intent1);
+        stopScan();
 
         return true;
     }
@@ -44,6 +97,9 @@ public class BLEService extends Service {
     {
         super.onCreate();
         mBLEState = BLEState.SCANNING;
+        mBluetoothManager = (BluetoothManager)  getSystemService(BLUETOOTH_SERVICE);
+        mBluetoothAdapter = mBluetoothManager.getAdapter();
+
     }
 
     @Override
@@ -55,12 +111,72 @@ public class BLEService extends Service {
     @Override
     public void onDestroy() {
         super.onDestroy();
+
+    }
+
+    public void startScan() {
+        mBluetoothLeScanner = mBluetoothAdapter.getBluetoothLeScanner();
+        mScanSettings = new ScanSettings.Builder().setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY).build();
+        mScanListFiler = new ArrayList<>();
+        mBluetoothLeScanner.startScan(mScanListFiler,mScanSettings,mScanCallback);
+        Log.d(TAG,"Started Scanning");
+
+    }
+
+    public void stopScan() {
+        mBluetoothLeScanner.stopScan(mScanCallback);
+        Log.d(TAG,"Stopped Scanning");
+
+    }
+
+    ScanCallback mScanCallback = new ScanCallback() {
+        @Override
+        public void onScanResult(int callbackType, ScanResult result) {
+            super.onScanResult(callbackType, result);
+            ScanRecord mScanRecord = result.getScanRecord();
+            int rssi = result.getRssi();
+            Log.d(TAG,"Scan record is " +mScanRecord);
+            Log.d(TAG,"RSSI is" +rssi);
+        }
+
+        @Override
+        public void onScanFailed(int err_code) {
+            super.onScanFailed(err_code);
+            Log.d(TAG,"Scan Failed");
+        }
+    };
+
+    public class BluetoothStateReceiver extends BroadcastReceiver
+    {
+
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            final String action = intent.getAction();
+            if (action.equals(BluetoothAdapter.ACTION_STATE_CHANGED)) {
+                final int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, BluetoothAdapter.ERROR);
+
+                switch(state) {
+                    case BluetoothAdapter.STATE_OFF:
+                        Log.d(TAG,"Turned off");
+                        Toast.makeText(getApplicationContext(),"Bluetooth Turned OFF",Toast.LENGTH_LONG).show();
+                        break;
+                    case BluetoothAdapter.STATE_ON:
+                        Log.d(TAG,"Turned ON");
+                        Toast.makeText(getApplicationContext(),"Bluetooth Turned ON",Toast.LENGTH_LONG).show();
+                        break;
+                }
+
+            }
+        }
     }
 
     public class BLEServiceBinder extends Binder {
         public void registerActivityMessenger(Messenger messenger){
             mMessenger = messenger;
         }
+
 
         public BLEState getBLEState() {
 
--- Automation/app/src/main/java/com/example/soumil/automation/MainActivity.java
+++ Automation/app/src/main/java/com/example/soumil/automation/MainActivity.java
@@ -1,9 +1,34 @@
 package com.example.soumil.automation;
 
+import android.Manifest;
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.bluetooth.BluetoothAdapter;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.PowerManager;
+import android.provider.Settings;
+import android.speech.tts.TextToSpeech;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -14,6 +39,8 @@ import android.widget.Toast;
 import com.daimajia.numberprogressbar.NumberProgressBar;
 import com.daimajia.numberprogressbar.OnProgressBarListener;
 
+import java.util.IllegalFormatException;
+import java.util.Locale;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -27,6 +54,20 @@ public class MainActivity extends AppCompatActivity implements OnProgressBarList
     private Button EndTestButton;
     private Button BLEConnectButton;
     private TextView textView;
+    private ServiceStateReceiver mServiceReceiver;
+    public TextToSpeech tts;
+
+    private static boolean mServiceBounded;
+    private static boolean mServiceRunning;
+    public static boolean mPermissionGranted;
+    LocationManager mLocationManager;
+    PowerManager mPowerManager;
+    private BLEService.BLEServiceBinder mServiceBinder;
+    public static final String TAG = MainActivity.class.getName();
+    private BluetoothAdapter mBluetoothAdapter;
+    private final Messenger mMessenger = new Messenger(new IncomingHandler());
+    NotificationManager notificationManager;
+    private static final int NOTIFY_ME_ID = 1336;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,12 +87,195 @@ public class MainActivity extends AppCompatActivity implements OnProgressBarList
         bnp = (NumberProgressBar) findViewById(R.id.numberbar1);
         bnp.setOnProgressBarListener(this);
         bnp.incrementProgressBy(1);
+        if (savedInstanceState != null)
+        {
+            mPermissionGranted = savedInstanceState.getBoolean("Permission");
+            mServiceBounded = savedInstanceState.getBoolean("Service Bounded");
+        }
+
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        mPowerManager = (PowerManager) getSystemService(Context.POWER_SERVICE);
+
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(this,"Bluetooth is not available", Toast.LENGTH_LONG).show();
+        }
+        tts =new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR) {
+                    tts.setLanguage(Locale.US);
+                    Log.d(TAG,"Language is set");
+                }
+            }
+        });
     }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState)
+    {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putBoolean("Permission",mPermissionGranted);
+        savedInstanceState.putBoolean("Service Bounded",mServiceBounded);
+
+    }
+    protected void display_notification(String data , int state)
+    {
+        String CHANNEL_ID = "my_channel_01";// The id of the channel.
+        CharSequence name = "Channel Name";// The user-visible name of the channel.
+        Notification.Builder mBuilder;
+        PendingIntent pendingIntent = null;
+        if (state == 0) {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class); // A new intent to launch another activity
+        /* getActivity has the following parameters
+        First - Context from which it is called
+        Second - RequestCode for the sender. Using it later will give us the same pending intent
+        Third - Intent object created before
+        Fourth - FLAG- This one is used to state if a previous pendingIntent already exists or not. If it does then
+        update it with the new intent. FLAG_UPDATE_CURRENT can be used for that.
+         */
+            pendingIntent = PendingIntent.getActivity(getApplicationContext(), (int) System.currentTimeMillis(), intent, 0);
+        }
+        else if (state == 1)
+        {
+            Intent myIntent = new Intent( Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            pendingIntent = PendingIntent.getActivity(getApplicationContext(), (int) System.currentTimeMillis(), myIntent, 0);
+            //context.startActivity(myIntent);
+        }
+        // Notification is created using the notification manager.
+        notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        int importance = NotificationManager.IMPORTANCE_HIGH;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel mChannel = new NotificationChannel(CHANNEL_ID, name, importance);
+            mChannel.enableLights(true);
+            mChannel.setLightColor(Color.RED);
+            mChannel.setShowBadge(true);
+            mChannel.setLockscreenVisibility(Notification.VISIBILITY_PUBLIC);
+            notificationManager.createNotificationChannel(mChannel);
+             mBuilder = new Notification.Builder(getApplicationContext(),CHANNEL_ID);
+        }
+        else
+        {
+             mBuilder = new Notification.Builder(getApplicationContext());
+
+        }
+        // This provides a builder interface to create a Notification object
+
+        // Setting this flag as true enables auto deletion of notification on clicking
+        mBuilder.setAutoCancel(true);
+        mBuilder.setContentText(data);
+        mBuilder.setContentTitle("BLE Service");
+        mBuilder.setContentIntent(pendingIntent);
+        mBuilder.setDefaults(Notification.DEFAULT_SOUND | Notification.DEFAULT_VIBRATE);
+        mBuilder.setNumber(7);
+        mBuilder.setChannelId(CHANNEL_ID);
+        mBuilder.setSmallIcon(R.drawable.example_picture);
+        Notification notification = mBuilder.build();
+        notificationManager.notify(NOTIFY_ME_ID, notification);
+    }
+
 
     @Override
     public void onResume()
     {
         super.onResume();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction("android.action.service");
+        filter.addAction("android.bluetooth.adapter.action.STATE_CHANGED");
+        filter.addAction("android.action.communication");
+        filter.addAction("android.action.destroyed");
+        filter.addAction("android.location.PROVIDERS_CHANGED");
+        filter.addAction(Intent.ACTION_SCREEN_ON);
+        filter.addAction(Intent.ACTION_SCREEN_OFF);
+
+        mServiceReceiver = new ServiceStateReceiver();
+        registerReceiver(mServiceReceiver, filter);
+
+        if (mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+            Log.d(TAG,"GPS is enabled");
+            if (notificationManager != null)
+                notificationManager.cancel(NOTIFY_ME_ID);
+        }
+        else
+        {
+            display_notification("GPS is disabled",1);
+            Log.d(TAG,"GPS is disabled");
+        }
+
+        if (!mBluetoothAdapter.enable()) {
+            Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(intent,1);
+        }
+        String permissions[] = {Manifest.permission.BLUETOOTH, Manifest.permission.BLUETOOTH_ADMIN, Manifest.permission.BLUETOOTH_PRIVILEGED, Manifest.permission.ACCESS_COARSE_LOCATION};
+        if (!mPermissionGranted)
+        {
+            ActivityCompat.requestPermissions(this, permissions,1);
+        }
+        else if (mPermissionGranted & mBluetoothAdapter.enable()) {
+            bindService(new Intent(this,BLEService.class),mServiceConnection,Context.BIND_AUTO_CREATE);
+        }
+
+    }
+    private void speakOut(String data) {
+
+        String text = data;
+        // tts = new TextToSpeech(this, this);
+        //tts.setLanguage(Locale.US);
+        tts.speak(text, TextToSpeech.QUEUE_ADD, null,null);
+    }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        if (mServiceBounded)
+            unbindService(mServiceConnection);
+        mServiceBounded = false;
+        try {
+            if (mServiceReceiver != null)
+                unregisterReceiver(mServiceReceiver);
+        }catch (IllegalFormatException e)
+        {
+        }
+
+
+    }
+
+    private ServiceConnection mServiceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            mServiceBinder = (BLEService.BLEServiceBinder) iBinder;
+            mServiceBinder.registerActivityMessenger(mMessenger);
+            mServiceBounded = true;
+            speakOut("Started Scanning");
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            mServiceBounded = false;
+
+        }
+    } ;
+
+    @Override
+    public void onRequestPermissionsResult(int resultCode, String permissions[], int[] grantResults)
+    {
+        switch(resultCode)
+        {
+            case 1:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {
+                    mPermissionGranted = true;
+                    bindService(new Intent(this,BLEService.class),mServiceConnection,Context.BIND_AUTO_CREATE);
+
+                }
+                else
+                {
+                    mPermissionGranted = false;
+                }
+                break;
+        }
+        return;
 
     }
 
@@ -77,7 +301,12 @@ public class MainActivity extends AppCompatActivity implements OnProgressBarList
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        timer.cancel();
+        if (tts != null) {
+            tts.stop();
+            tts.shutdown();
+        }
+
+
     }
 
     @Override
@@ -93,7 +322,6 @@ public class MainActivity extends AppCompatActivity implements OnProgressBarList
         switch(view.getId())
         {
             case R.id.cancelTestButton:
-
                 break;
             case R.id.ConnectButton:
                 break;
@@ -103,6 +331,51 @@ public class MainActivity extends AppCompatActivity implements OnProgressBarList
                 break;
             case R.id.RestartTestButton:
                 break;
+        }
+    }
+
+    public class IncomingHandler extends Handler
+    {
+        @Override
+        public void handleMessage(Message message)
+        {
+            int responsecode = message.what;
+            switch(responsecode)
+            {
+                default:
+                    super.handleMessage(message);
+            }
+        }
+
+    }
+
+    public class ServiceStateReceiver extends BroadcastReceiver
+    {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals("android.action.service"))
+            {
+                mServiceBounded = false;
+                Toast.makeText(getApplicationContext(), "Service is unbinded",Toast.LENGTH_SHORT).show();
+                speakOut("Stopped Scanning");
+
+            }
+
+            if (intent.getAction().matches("android.location.PROVIDERS_CHANGED")) {
+
+                Log.d(TAG,"Location Providers Changed");
+                if (mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+                    Log.d(TAG,"GPS is enabled");
+                    if (notificationManager!= null)
+                        notificationManager.cancel(NOTIFY_ME_ID);
+                }
+                else
+                {
+                    display_notification("GPS is disabled",1);
+                    Log.d(TAG,"GPS is disabled");
+                }
+            }
+
         }
     }
 }
